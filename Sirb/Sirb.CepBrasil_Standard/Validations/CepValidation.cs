76d10ce0ded40caac6cc84e124b97671a809c1f2
Rodrigo Araujo Barbosa <rodrigoarabarbosa@outlook.com>
2022-09-21 17:41:20 -0300

    Add yml for sonar

--- Sirb/Sirb.CepBrasil_Standard/Validations/CepValidation.cs
+++ Sirb/Sirb.CepBrasil_Standard/Validations/CepValidation.cs
@@ -2,21 +2,20 @@ using Sirb.CepBrasil_Standard.Exceptions;
 using Sirb.CepBrasil_Standard.Extensions;
 using Sirb.CepBrasil_Standard.Messages;
 
-namespace Sirb.CepBrasil_Standard.Validations
-{
-    internal class CepValidation
-    {
-        private const int ZipCodeLength = 8;
+namespace Sirb.CepBrasil_Standard.Validations;
 
-        /// <summary>
-        /// Validate brazilian zip code to its minimum value standard.
-        /// </summary>
-        /// <param name="zipCode"></param>
-        public static void Validate(string zipCode)
-        {
-            string value = zipCode?.RemoveMask();
-            int valueLength = value?.Length ?? 0;
-            ServiceException.ThrowIf(valueLength != ZipCodeLength, CepMessage.ZipCodeInvalidMessage);
-        }
+internal static class CepValidation
+{
+    private const int ZipCodeLength = 8;
+
+    /// <summary>
+    /// Validate brazilian zip code to its minimum value standard.
+    /// </summary>
+    /// <param name="zipCode"></param>
+    public static void Validate(string zipCode)
+    {
+        var value = zipCode?.RemoveMask();
+        var valueLength = value?.Length ?? 0;
+        ServiceException.ThrowIf(valueLength != ZipCodeLength, CepMessage.ZipCodeInvalidMessage);
     }
 }
